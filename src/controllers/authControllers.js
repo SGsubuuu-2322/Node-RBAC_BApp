2adb01df3d60b57cddff4660161709acf7c9ee8e
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-27 02:19:34 +0530

    Debug some code..

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -19,7 +19,35 @@ const register = async (req, res) => {
   }
 };
 const login = async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: `User with username ${username} not found` });
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      return res.status(400).json({
+        message: `Username or Password not matched. Try again...`,
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    return res.status(500).json({
+      message: `Something went wrong... ${err}`,
+    });
+  }
 };
 
 module.exports = {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { register, login } = require("../controllers/authControllers");
 
 router.post("/register", register);
-router.post("/login", register);
+router.post("/login", login);
 
 module.exports = router;
